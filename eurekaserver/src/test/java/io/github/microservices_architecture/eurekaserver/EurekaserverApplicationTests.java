87a514888447e3d9959da2742c9565f7f1113a89
ViniciusAlvesdosSantos <vinicius.santos@sptech.school>
2026-01-15 15:57:03 -0300

    initialize project structure with Maven and Spring Boot setup

---
+++ eurekaserver/src/test/java/io/github/microservices_architecture/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.microservices_architecture.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
